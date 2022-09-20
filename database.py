7b85a37b304a23b637186469d568108336e1ed9e
shakedyk4 <shakedyk@gmail.com>
2022-09-20 10:00:39 +0300

    working new

--- database.py
+++ database.py
@@ -31,18 +31,48 @@ with open('models_new.csv', 'w') as newFile:
     myDictWriter.writeheader()
 
 import pygame
-
+# import pandas as pd
+# import numpy as pd
+import csv
 import consts
 import minefield
 
+mylist = []
 
-# import pandas as pd
+
+def read_csv(file):
+    with open(file, 'r') as game:
+        myReader = csv.reader(game)
+        # next(myReader)
+        mylist = list(myReader)
+
+    for myItem in mylist:
+        print(myItem)
+
+
+# read_csv()
+
+
+# with open('games_theFlag.csv', 'r') as myFile:
+#     myDictReader = csv.DictReader(myFile)
+#     myList = list(myDictReader)
+
+
+def new_csv(index, data):
+    with open('game.csv', 'a') as newFile:
+        # myDictWriter = csv.DictWriter(newFile, [index * "\n", data])
+        # create the csv writer
+        writer = csv.writer(newFile)
+        # write a row to the csv file
+        writer.writerow([data])
+
 
 # checking which number is pressed
 def num_press_less(keys_pressed, soldier_rect, field):
     for num_key in range(len(consts.NUMBERS_KEYS)):
         if keys_pressed[consts.NUMBERS_KEYS[num_key]]:
             data = create_data(num_key + 1, soldier_rect, field)
+            new_csv(num_key + 1, data)
 
 
 def create_data(key_num, soldier_rect, field):
@@ -68,4 +98,3 @@ def num_press_more(keys_pressed):
         pass
     elif keys_pressed[pygame.K_9]:
         pass
-
